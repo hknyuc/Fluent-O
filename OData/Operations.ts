f1617a2287dc88b99284f5b3aa1b92fe4ca93c08
hakan <hakan.yucel@alisangroup.com>
2018-08-03 11:05:38 +0300

    decorate set function created

--- OData/Operations.ts
+++ OData/Operations.ts
@@ -2,6 +2,7 @@ import { Guid } from './Schema';
 import { MemSet } from './MemArrayVisitor';
 import {ODataSet} from './OData';
 import { Select, Filter, Count, EqBinary, Operation, Property, Top, Skip, Expand, Order, InlineCount, Value, ModelMethod, Root, This, SelectMany, It, Find, GlobalMethod, Method, Action, Func } from './Expressions';
+import { DecorateSet } from './Context';
 
 export class EqBinaryExtend extends EqBinary {
     constructor(eqBinary: EqBinary) {
@@ -1149,4 +1150,8 @@ export function func(name:string,...params){
         return true;
     });
     return new Func(name,args);
+}   
+
+export function dataset(source,observer:{get:Function,add:Function,delete:Function,query:Function,update:Function,addUpdate:Function}){
+   return new DecorateSet(source,observer);
 }
