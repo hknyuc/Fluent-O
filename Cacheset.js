80249c998f1e0d31b2731e42c509540535da1207
hakan <hakan.yucel@alisangroup.com>
2018-09-04 18:25:01 +0300

    no message

--- Cacheset.js
+++ Cacheset.js
@@ -2,28 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Dataset_1 = require("./Dataset");
 const OData_1 = require("./OData");
-class CacheSet extends Dataset_1.DecorateSet {
+class CacheSet extends Dataset_1.DataSet {
     constructor(dataset) {
         if (dataset == null)
             throw new Error('dataset is null for caching');
-        let self;
-        super(dataset, {
-            get: function (expressions) {
-                self.caches = self.caches || {};
-                let query = OData_1.QuerySet.get(expressions);
-                if (self.caches[query] != null) {
-                    return Promise.resolve(self.caches[query]);
-                }
-                return this.next().then((response) => {
-                    self.caches[query] = response;
-                    return response;
-                });
-            }
-        });
-        self = this;
+        super();
+        this.dataset = dataset;
     }
-    query(expressions) {
-        let result = new CacheSet(this.dataSet.query.apply(this.dataSet, arguments));
+    get(expressions) {
+        this.caches = self.caches || {};
+        let query = OData_1.QuerySet.get(expressions);
+        if (self.caches[query] != null) {
+            return Promise.resolve(self.caches[query]);
+        }
+        return this.dataset.get.apply(this.dataset, arguments).then((response) => {
+            self.caches[query] = response;
+            return response;
+        });
+    }
+    query() {
+        let result = new CacheSet(this.dataset.query.apply(this.dataset, arguments));
         result.caches = this.caches;
         return result;
     }
--- Cacheset.ts
+++ Cacheset.ts
@@ -1,29 +1,29 @@
-import { DecorateSet, DataSet } from "./Dataset";
+import { DataSet } from "./Dataset";
 import { QuerySet } from "./OData";
 
-export class CacheSet<T> extends DecorateSet<T>{
+export class CacheSet<T> extends DataSet<T>{
     private caches;
+    private dataset:DataSet<T>
     constructor(dataset:DataSet<T>){
         if(dataset == null) throw new Error('dataset is null for caching');
-        let self;
-        super(dataset,{
-            get:function (expressions){
-                self.caches = self.caches || {};
-                let query = QuerySet.get(expressions);
-                if(self.caches[query] != null){ 
-                    return Promise.resolve(self.caches[query]);
-                }
-                return this.next().then((response)=>{
-                     self.caches[query] = response;
-                     return response;
-                });
-            }
-        });
-        self = this;
+        super();
+        this.dataset = dataset;
     }
 
-    query(expressions){
-      let result = new CacheSet(this.dataSet.query.apply(this.dataSet,arguments));
+    get(expressions){
+        this.caches = self.caches || {};
+        let query = QuerySet.get(expressions);
+        if(self.caches[query] != null){ 
+            return Promise.resolve(self.caches[query]);
+        }
+        return this.dataset.get.apply(this.dataset,arguments).then((response)=>{
+            self.caches[query] = response;
+            return response;
+        });
+    }
+
+    query(){
+      let result = new CacheSet(this.dataset.query.apply(this.dataset,arguments));
       result.caches = this.caches;
       return result;
     }
